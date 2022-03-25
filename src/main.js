78d0b7d771a2bdbc8b0d5fc3b44602cfd624c110
peterroe <peterroe@163.com>
2022-03-25 13:42:26 +0800

    feat dep

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import UngeUI from 'ungeui'
 
+//ungeui
+import UngeUI from 'ungeui'
 import 'ungeui/dist/style.css'
 
 createApp(App).use(UngeUI).mount('#app')
