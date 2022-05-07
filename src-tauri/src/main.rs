16dbfb36573fd332876399ec8270f74b62a77d6e
peterroe <linshuheng@bytedance.com>
2022-05-07 23:24:47 +0800

    update(deps): upgrade

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,21 +10,4 @@ fn main() {
   .run(tauri::generate_context!())
   .expect("error while running tauri application");
   
-  run();
-}
-
-async fn run() {
-  println!("Hello, world!");
-  let client = ClientBuilder::new()
-    .max_redirections(3)
-    .build()
-    .unwrap();
-  let mut request_builder = HttpRequestBuilder::new("GET","https://rsshub.app/axis-studios/work/full-service-cg-production").unwrap();
-  let request = request_builder.response_type(ResponseType::Text);
-
-  if let Ok(response) = client.send(request).await {
-    println!("hello, world!");
-  } else {
-    println!("error");
-  }
 }
