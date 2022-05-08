9db8094b354f4aed5ecbe06573b7aa4529a252f1
peterroe <linshuheng@bytedance.com>
2022-05-08 22:56:11 +0800

    feat: macOs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,6 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::api::http::{ HttpRequestBuilder, ResponseType, ClientBuilder };
-
 fn main() {
   tauri::Builder::default()
   .run(tauri::generate_context!())
